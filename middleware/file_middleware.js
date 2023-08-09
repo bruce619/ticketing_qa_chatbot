3a7a56c710b5ca5634ca05adecea9b86bf74f3e3
Ibecheozor Chimuanya Bruce <chimuanyaibechozor@gmail.com>
2023-08-09 14:22:51 +0100

    implemented image handling in chat

--- middleware/file_middleware.js
+++ middleware/file_middleware.js
@@ -15,15 +15,15 @@ const storage = multer.diskStorage({
   const file_upload = multer({
     storage: storage,
     fileFilter: function(req, file, cb){
-        const validFileExtentions = [".jpg", ".png", ".jpeg"]
+        const validFileExtentions = [".jpg", ".JPG", ".png", ".PNG", ".jpeg", ".JPEG"]
         const ext_ = path.extname(file.originalname);
         if (!validFileExtentions.includes(ext_)){
-            return cb(new Error("Invalid file. Please select either .jpg, .png, or .jpeg"))
+            return cb(new Error("Invalid file. Please select either .jpg, .JPG, .png, .PNG, .jpeg, .JPEG"))
         }
         cb(null, true)
     },
-    limits: {fileSize: 125000 * 10}, // 10mb
-  });
+    limits: { fileSize: 125000 * 20 }, // 20mb
+});
 
 
 module.exports = file_upload
